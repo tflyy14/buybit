97af440f8d8ce0981735cd61fd9d2106af52955b
yy <tflyy14@163.com>
2018-09-28 17:27:50 +0800

    Initial commit

---
+++ src/main/java/com/buybit/test/Test.java
@@ -0,0 +1,13 @@
+package com.buybit.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        sayHello();
+    }
+
+    public static void sayHello(){
+        System.out.println("hello world   测试中文 ");
+    }
+}
